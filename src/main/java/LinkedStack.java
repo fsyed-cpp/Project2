81c6e1a74b26987c612f779fc4e43498f7dc4dd0
Musa Waghu <mtwaghu@cpp.edu>
2022-03-08 16:54:46 -0800

    Finished the skeleton code for StackInterface and LinkedStack

--- src/main/java/LinkedStack.java
+++ src/main/java/LinkedStack.java
@@ -1,3 +1,63 @@
-public class LinkedStack {
-    // TODO...
+import java.util.EmptyStackException;
+
+public class LinkedStack<T> implements StackInterface<T> {
+    private Node topNode;
+
+    public LinkedStack() {
+        topNode = null;
+    }
+
+    private class Node {
+        private T data;
+        private Node next;
+
+        private Node(T dataPortion) {
+            this(dataPortion, null);
+        }
+
+        private Node(T dataPortion, Node linkPortion) {
+            data = dataPortion;
+            next = linkPortion;
+        }
+
+        private T getData() {
+            return data;
+        }
+
+        private void setData(T newData) {
+            data = newData;
+        }
+
+        private Node getNextNode() {
+            return next;
+        }
+
+        private void setNextNode(Node nextNode) {
+            next = nextNode;
+        }
+    }
+
+    public void push (T newEntry) {
+        topNode = new Node(newEntry, topNode);
+    }
+
+    public T pop() {
+        T top = peek();
+        topNode = topNode.getNextNode();
+        return top;
+    }
+    public T peek() {
+        if (isEmpty())
+            throw new EmptyStackException();
+        else
+            return topNode.getData();
+    }
+
+    public boolean isEmpty() {
+        return topNode == null;
+    }
+    public void clear() {
+        topNode = null;
+    }
 }
+
