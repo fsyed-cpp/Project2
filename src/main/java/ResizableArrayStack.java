2349455d362000a00c35db811016a3f578f3b023
rariggs@cpp.edu <rariggs@cpp.edu>
2022-03-10 02:53:31 -0800

    adding default constructor and overloading constructor

--- src/main/java/ResizableArrayStack.java
+++ src/main/java/ResizableArrayStack.java
@@ -1,7 +1,30 @@
 import java.lang.SuppressWarnings;
 import java.util.Arrays;
 
+/** A class of stacks whose entries are stored in an array. */
 public abstract class ResizableArrayStack <T> implements StackInterface<T> {
+
+    private T[] stack; // Array of stack entries
+    private int topIndex; //Index of top entry
+    private boolean integrityOK = false;
+    private static final int DEFAULT_CAPACITY = 50;
+    private static final int MAX_CAPACITY = 10000;
+
+    public ResizableArrayStack() {
+        this(DEFAULT_CAPACITY);
+    } // end default constructor
+
+    public ResizableArrayStack(int initialCapacity) {
+        integrityOK = false;
+        //checkCapacity(initialCapacity);
+
+        // The cast is safe because the new array contains null entries
+      //  @SuppressWarnings("unchecked")
+       // T[] tempStack = (T[])new Object(InitialCapacity);
+       // stack = tempStack;
+        topIndex = -1;
+        integrityOK = true;
+    } // end constructor
 
     @Override
     public void push(T newEntry)
