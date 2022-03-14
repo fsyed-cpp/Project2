2a8de4feec5504e426d5f18c553ad48ae8690bb7
rariggs@cpp.edu <rariggs@cpp.edu>
2022-03-14 03:12:50 -0700

    Adding test files structures

--- src/test/java/LinkedStackTest.java
+++ src/test/java/LinkedStackTest.java
@@ -1,4 +1,23 @@
 import junit.framework.TestCase;
 public class LinkedStackTest extends TestCase {
-  
+
+    public void testPush()
+    {
+    }
+
+    public void testPop()
+    {
+    }
+
+    public void testPeek()
+    {
+    }
+
+    public void testIsEmpty()
+    {
+    }
+
+    public void testClear()
+    {
+    }
 }
