3ff2dd9e817f463254e2cbece2553a7c9e852ffd
rariggs@cpp.edu <rariggs@cpp.edu>
2022-03-17 17:44:50 -0700

    Please ignore this commit

--- src/test/java/LinkedStackTest.java
+++ src/test/java/LinkedStackTest.java
@@ -1,23 +1,36 @@
 import junit.framework.TestCase;
 public class LinkedStackTest extends TestCase {
 
+    @Override
+    public void setUp() throws Exception
+    {
+        super.setUp();
+    }
+
+
     public void testPush()
     {
+
+
     }
 
     public void testPop()
     {
+
     }
 
     public void testPeek()
     {
+
     }
 
     public void testIsEmpty()
     {
+
     }
 
     public void testClear()
     {
+
     }
 }
