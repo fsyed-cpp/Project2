daa9fc6952303e1f042951ab4147a58ba99fc64a
fsyed-cpp <fsyed@cpp.edu>
2022-03-19 20:04:36 -0700

    Added test case for evaluatePostfix(), added extra logic to sanitize user input for evaluatePostfix()

--- src/test/java/LinkedStackTest.java
+++ src/test/java/LinkedStackTest.java
@@ -1,5 +1,5 @@
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedStackTest {
 
@@ -27,7 +27,32 @@ public class LinkedStackTest {
     public void convertToPostfix() {
     }
 
+    /**
+     * Tests the evaluatePostfix method to ensure we are properly parsing postfix expressions
+     * Test Case 1: Postfix expression equal to 33
+     * Test Case 2: Postfix expression equal to 16
+     * Test Case 3: Invalid input
+     */
     @Test
-    public void evaluatePostfix() {
+    void testEvaluatePostfix() {
+
+        // Arrange
+        ResizableArrayStack<Integer> arrayStack = new ResizableArrayStack<>();
+        String postfixExpression = "2 3 * 4 2 - / 5 6 * +"; // Should be 33
+        String postfixExpression2 = "2 5 3 6 + * * 5 / 2 -"; // Should be 16
+        String postfixExpressionError = "";
+
+        // Act
+        float arrayResult = arrayStack.evaluatePostfix(postfixExpression);
+        float arrayResult2 = arrayStack.evaluatePostfix(postfixExpression2);
+        float errorResult = arrayStack.evaluatePostfix(postfixExpressionError);
+        float expectedArrayResult = 33;
+        float expectedArrayResult2 = 16;
+        float expectedErrorResult = -404;
+
+        // Assert
+        assertEquals(arrayResult, expectedArrayResult);
+        assertEquals(arrayResult2, expectedArrayResult2);
+        assertEquals(errorResult, expectedErrorResult);
     }
 }
